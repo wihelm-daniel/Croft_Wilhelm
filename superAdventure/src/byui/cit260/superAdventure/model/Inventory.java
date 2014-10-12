ef3fe6aced55864629c903beac445dc53e0921eb
Brad <Brad@step-up>
2014-10-11 22:08:17 -0600

    Worked with Dan Wilhelm, Dan walked me through on creating the Keys.java file and modifying SuperAdventure.Java file.
    10/11/14
    Added the Inventory Class.

---
+++ superAdventure/src/byui/cit260/superAdventure/model/Inventory.java
@@ -0,0 +1,83 @@
+
+package byui.cit260.superAdventure.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+public class Inventory implements Serializable{
+    
+    private String item;
+    private int used;
+    private int quantity;
+
+    public Inventory() {
+    }
+
+       
+    
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public int getUsed() {
+        return used;
+    }
+
+    public void setUsed(int used) {
+        this.used = used;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Inventory{" + "item=" + item + ", used=" + used + ", quantity=" + quantity + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 71 * hash + Objects.hashCode(this.item);
+        hash = 71 * hash + this.used;
+        hash = 71 * hash + this.quantity;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Inventory other = (Inventory) obj;
+        if (!Objects.equals(this.item, other.item)) {
+            return false;
+        }
+        if (this.used != other.used) {
+            return false;
+        }
+        if (this.quantity != other.quantity) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+    
+   
+    
+    
+}
