070efac21a6be2dcc9c33c8fef013e87b4da0fc1
Dan Wilhelm <wil12045@byui.edu>
2014-10-22 21:05:47 -0700

    There are some errors in the test cases. Not sure what the problem is.

--- superAdventure/src/byui/cit260/superAdventure/control/RoomObjectControl.java
+++ superAdventure/src/byui/cit260/superAdventure/control/RoomObjectControl.java
@@ -2,7 +2,7 @@ package byui.cit260.superAdventure.control;
 
 public class RoomObjectControl {
 
-    public double checkLockCover(double radius, double height) {
+    public float checkLockCover(int radius, int height) {
 
         if (radius < 1) {
             System.out.println("Enter a number between 1.0 and 8.0");
@@ -11,15 +11,15 @@ public class RoomObjectControl {
         if (radius > 8) {
             System.out.println("Enter a number less than 8.0");
         }
-
-        double volumeLow = 1; // The other class doesn't exist yet, this is just a temporary holder.
-        double volumeHigh = 540.0; //The other class doesn't exist yet, this is just a temporary holder.
-        double answer = Math.PI * (Math.pow(radius, 2) * (height / 3));
-
+        
+        float volumeLow = 1; // The other class doesn't exist yet, this is just a temporary holder.
+        float volumeHigh = 536; //The other class doesn't exist yet, this is just a temporary holder.
+        float volume = (float)((float) Math.PI * (Math.pow(radius, 2) * (height / 3)));
+        int answer = Math.round(volume);
         if (answer >= volumeLow && answer <= volumeHigh) {
             return answer;
 
-        } else if (answer < volumeLow) {
+        } else if (volume < volumeLow) {
             System.out.println("Volume too low. Sand dumped out.");
 
             return 0;
--- superAdventure/test/byui/cit260/superAdventure/control/RoomObjectControlTest.java
+++ superAdventure/test/byui/cit260/superAdventure/control/RoomObjectControlTest.java
@@ -10,27 +10,125 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author Brad Croft
+ * @author Brad Croft/Edited by Dan Wilhelm
  */
 public class RoomObjectControlTest {
     
     public RoomObjectControlTest() {
     }
-
-    /**
-     * Test of checkLockCover method, of class RoomObjectControl.
-     */
-    @Test
-    public void testCheckLockCover() {
-        System.out.println("checkLockCover");
-        double radius = 8.0;
-        double height = 8.0;
+    
+   @Test
+   
+   public void testCheckLockCover() {
+     System.out.println("checkLockCover");
+   /*
+    *  Test Case #1
+    */
+        System.out.println("\t Test #1");
+        
+        int radius = 1;
+        int height = 1;
         RoomObjectControl instance = new RoomObjectControl();
-        double expResult = 536.1651462126933;
+        int expResult = 1;
         double result = instance.checkLockCover(radius, height);
-        assertEquals(expResult, result, 0.0);
+        assertEquals(expResult, result, 1);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
+        
+   /*
+    *  Test Case #2
+    */
+        System.out.println("\t Test #2");
+        
+        radius = 8;
+        height = 8;
+        expResult = 536;
+        result = instance.checkLockCover(radius, height);
+        assertEquals(expResult, result, 1);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+        
+   /*
+    *  Test Case #3
+    */
+        System.out.println("\t Test #3");
+        
+        radius = 0;
+        height = 0;
+        expResult = -1;
+        result = instance.checkLockCover(radius, height);
+        assertEquals(expResult, result, 1);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+        
+          
+   /*
+    *  Test Case #4
+    */
+        System.out.println("\t Test #4");
+       
+        radius = 9;
+        height = 9;
+        expResult = -1;
+        result = instance.checkLockCover(radius, height);
+        assertEquals(expResult, result, 1);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+        
+   /*
+    *  Test Case #5
+    */
+        System.out.println("\t Test #5");
+        
+        radius = 1;
+        height = 1;
+        expResult = 1;
+        result = instance.checkLockCover(radius, height);
+        assertEquals(expResult, result, 1);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+         /*
+    *  Test Case #6
+    */
+        System.out.println("\t Test #6");
+        
+        radius = 1;
+        height = 8;
+        expResult = 8;
+        result = instance.checkLockCover(radius, height);
+        assertEquals(expResult, result, 1);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+        
+   /*
+    *  Test Case #7
+    */
+        System.out.println("\t Test #7");
+        
+        radius = 8;
+        height = 67;
+        expResult = 536;
+        result = instance.checkLockCover(radius, height);
+        assertEquals(expResult, result, 1);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+        
+   /*
+    *  Test Case #8
+    */
+        System.out.println("\t Test #8");
+        
+        radius = 8;
+        height = 8;
+        expResult = 536;
+        result = instance.checkLockCover(radius, height);
+        assertEquals(expResult, result, 1);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+        
+    
     }
+
+    
     
 }
