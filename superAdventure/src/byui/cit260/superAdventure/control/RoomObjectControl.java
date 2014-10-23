ca2dd5a1224eb2d3f70b9384cc8271b31e9fec76
Brad <Brad@step-up>
2014-10-22 21:10:32 -0600

    10/22 Worked on adding RoomObjectControl class and testing with Junit.

---
+++ superAdventure/src/byui/cit260/superAdventure/control/RoomObjectControl.java
@@ -0,0 +1,32 @@
+package byui.cit260.superAdventure.control;
+
+public class RoomObjectControl {
+
+    public double checkLockCover(double radius, double height) {
+
+        if (radius < 1) {
+            System.out.println("Enter a number between 1.0 and 8.0");
+            return -1;
+        }
+        if (radius > 8) {
+            System.out.println("Enter a number less than 8.0");
+        }
+
+        double volumeLow = 1; // The other class doesn't exist yet, this is just a temporary holder.
+        double volumeHigh = 540.0; //The other class doesn't exist yet, this is just a temporary holder.
+        double answer = Math.PI * (Math.pow(radius, 2) * (height / 3));
+
+        if (answer >= volumeLow && answer <= volumeHigh) {
+            return answer;
+
+        } else if (answer < volumeLow) {
+            System.out.println("Volume too low. Sand dumped out.");
+
+            return 0;
+        } else {
+            System.out.println(" Volume too high. Sand dumped out");
+            return 0;
+
+        }
+    }
+}
--- superAdventure/src/superadventure/SuperAdventure.java
+++ superAdventure/src/superadventure/SuperAdventure.java
@@ -1,6 +1,7 @@
-
 package superadventure;
 
+import byui.cit260.superAdventure.model.Game;
+import byui.cit260.superAdventure.model.Inventory;
 import byui.cit260.superAdventure.model.Keys;
 import byui.cit260.superAdventure.model.Location;
 import byui.cit260.superAdventure.model.Locks;
@@ -11,27 +12,24 @@ import byui.cit260.superAdventure.model.Villains;
 import java.util.HashSet;
 import java.util.Set;
 
-
 public class SuperAdventure {
 
-    
     public static void main(String[] args) {
-      
+
         //Dan
         Player playerOne = new Player();
         playerOne.setName("Fred Flinstone");
         playerOne.setTime(7.00);
         String playerInfo = playerOne.toString();
         System.out.println(playerInfo);
-        
-        
+
+        //Brad
         Keys keysOne = new Keys();
-        
         keysOne.setKeyShape("Circle");
         String keyInfo = keysOne.toString();
-        
+
         System.out.println(keyInfo);
-        
+
         //Dan
         Location loc = new Location();
         loc.setRow(4);
@@ -40,7 +38,7 @@ public class SuperAdventure {
         loc.setRoomObject(7);
         String locInfo = loc.toString();
         System.out.println(locInfo);
-        
+
         //Dan
         RoomObject rObject = new RoomObject();
         rObject.setType(2);
@@ -50,7 +48,7 @@ public class SuperAdventure {
         rObject.setTreasure(14);
         String rObjectInfo = rObject.toString();
         System.out.println(rObjectInfo);
-        
+
         //Dan
         Villains vil = new Villains();
         vil.setWeakness(4);
@@ -58,14 +56,14 @@ public class SuperAdventure {
         vil.setSuccessMessage("Asto is down for the count!");
         String vilInfo = vil.toString();
         System.out.println(vilInfo);
-        
+
         //Dan
         Puzzles puz = new Puzzles();
         puz.setQuestion("What is 4^2");
         puz.setAnswer("16");
-        puz.setHints("It rhymes with ixteen");        
+        puz.setHints("It rhymes with ixteen");
         puz.setSuccessMessage("You are, like, wicked smart");
-        
+
         //Dan
         Locks lock = new Locks();
         lock.setLockShape("Oval");
@@ -73,6 +71,24 @@ public class SuperAdventure {
         lock.setTreasure(4);
         String lockInfo = lock.toString();
         System.out.println(lockInfo);
+
+        //Brad
+        Inventory inv = new Inventory();
+        inv.setItem("Sword");
+        inv.setUsed(4);
+        inv.setQuantity(2);
+        String invDetails = inv.toString();
+        System.out.println(invDetails);
+
+        //Brad
+        Game gameClass = new Game();
+        gameClass.setNameOfGame("Super Adventure");
+        gameClass.setNumOfMaps("5 Levels");
+        gameClass.setRoomObjects("Various Objects are found in each room");
+        gameClass.setInventoryItemCount("Each player can have various amounts of inventory that they carry");
+        String gameDetails = gameClass.toString();
+        System.out.println(gameDetails);
+
     }
-    
+
 }
